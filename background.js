12924fb3293f3458dd39daf71db48e1fec4c70dd
Rusiru Erandaka <erandak-ec20037@stu.kln.ac.lk>
2025-01-29 00:00:08 +0530

    chrome authenticate part commented

--- background.js
+++ background.js
@@ -34,11 +34,11 @@ chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
   }
 });
 
-chrome.identity.getAuthToken({ interactive: true }, function(token) {
-  if (chrome.runtime.lastError || !token) {
-    console.error("Authentication failed: " + chrome.runtime.lastError.message);
-    return;
-  }
+//chrome.identity.getAuthToken({ interactive: true }, function(token) {
+  //if (chrome.runtime.lastError || !token) {
+//console.error("Authentication failed: " + chrome.runtime.lastError.message);
+//return;
+ // }
   // Use the token to authenticate Google Drive upload
-});
+//});
 
